d62709206348ea3ac6415786e10144b85ba7202c
akatsukinoyami <akatsukinoyami@yahoo.com>
2022-11-07 13:13:49 +0200

    added captions to image modals

--- src/markdown.parser.ts
+++ src/markdown.parser.ts
@@ -44,12 +44,13 @@ function imageUrlExtension(path: string): ShowdownExtensionType{
                 <a class="button_modal" href="#$2_modal">
                   <img $1 src="${path}/$2" $3 alt="$4" $5 style="object-fit: contain;" />
                 </a>
-                <figcaption class="figure-caption text-center" style="font-family: 'Ubuntu', Helvetica, Verdana, sans-serif;"> $4 </figcaption>
+                <figcaption class="figure-caption modal_caption text-center"> $4 </figcaption>
               </figure>
-              <div id="$2_modal" class="overlay light">
+              <div id="$2_modal" class="overlay light rounded">
                 <div class="popup d-inline-block">
-                  <a class="close" href="#" onclick="document.getElementById('$2').scrollIntoView()">&times;</a>
+                  <a class="close " href="#" onclick="document.getElementById('$2').scrollIntoView()">&times;</a>
                   <img $1 src="${path}/$2" $3 alt="$4" $5 style="object-fit: contain; height: 100%" />
+                  <p class="figure-caption modal_caption text-center"> $4 </p>
                 </div>
               </div>
 `
@@ -60,7 +61,7 @@ function classBindings(): ShowdownExtensionType[]{
   const classMap = Object.entries({
     pre: "bg-secondary bg-opacity-10 rounded p-3",
     blockquote: "blockquote border-2 border-start border-success rounded p-2",
-    img: "rounded img-fluid",
+    img: "rounded img-fluid pb-2",
     h1: 'pt-3', h2: 'pt-3',
     h3: 'pt-3', h4: 'pt-2 fs-5',
     h5: 'pt-2 fs-6',
