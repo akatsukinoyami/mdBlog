db73686fe1652bc90f6f278fd51ae398f7d05692
akatsukinoyami <akatsukinoyami@yahoo.com>
2022-11-10 00:07:55 +0200

    refactored markdown parser

--- src/markdown.parser.ts
+++ src/markdown.parser.ts
@@ -1,5 +1,5 @@
-import showdown from 'showdown';
-import type ShowdownExtensionType from './types/showdown.extension.type';
+import showdown from "showdown";
+import type ShowdownExtensionType from "./types/showdown.extension.type";
 
 export default function parseMarkdown(link: string, markdown: string): string {
     return getParser(link).makeHtml(markdown);
@@ -7,7 +7,7 @@ export default function parseMarkdown(link: string, markdown: string): string {
 
 function getParser(link: string): showdown.Converter {
   return new showdown.Converter({
-    flavor: 'github',
+    flavor: "github",
     ghCodeBlocks:  true,
     ghCompatibleHeaderId:  true,
     omitExtraWLInCodeBlocks:  true,
@@ -18,19 +18,19 @@ function getParser(link: string): showdown.Converter {
     tasklists:  true,
     extensions: [
       replaceReturnWithBr(),
-      classBindings(),
+      outputBindings(),
       imageUrlExtension(filterLinkParent(link))
     ]});
 }
 function filterLinkParent(link: string): string{
-  const pathSplitted = link.split('/');
+  const pathSplitted = link.split("/");
   pathSplitted.pop();
-  return pathSplitted.join('/');
+  return pathSplitted.join("/");
 };
 
 function replaceReturnWithBr(): ShowdownExtensionType{
   return {
-    type: 'lang',
+    type: "lang",
     regex: /\\n/g,
     replace: `<br>`
   };
@@ -38,7 +38,7 @@ function replaceReturnWithBr(): ShowdownExtensionType{
 
 function imageUrlExtension(path: string): ShowdownExtensionType{
   return {
-    type: 'output',
+    type: "output",
     regex: /<img(.*?)src="(.*?)"(.*?)alt="(.*?)"(.*?)>/g,
     replace: `<figure class="figure w-100" id="$2">
                 <a class="button_modal" href="#$2_modal">
@@ -57,20 +57,20 @@ function imageUrlExtension(path: string): ShowdownExtensionType{
   };
 }
 
-function classBindings(): ShowdownExtensionType[]{
+function outputBindings(): ShowdownExtensionType[]{
   const classMap = Object.entries({
     pre: "bg-secondary bg-opacity-10 rounded p-3",
     blockquote: "blockquote border-2 border-start border-success rounded p-2",
     img: "rounded img-fluid pb-2",
-    h1: 'pt-3', h2: 'pt-3',
-    h3: 'pt-3', h4: 'pt-2 fs-5',
-    h5: 'pt-2 fs-6',
-    table: 'table',
+    h1: "pt-3", h2: "pt-3",
+    h3: "pt-3", h4: "pt-2 fs-5",
+    h5: "pt-2 fs-6", p: "px-2",
+    table: "table"
   })
   const extensions = classMap.map((tag) => {
     return {
-      type: 'output',
-      regex: new RegExp(`<${tag[0]}(.*)>`, 'g'),
+      type: "output",
+      regex: new RegExp(`<${tag[0]}(.*?)>`, 'g'),
       replace: `<${tag[0]} class="${tag[1]}" $1>`
     }
   })
