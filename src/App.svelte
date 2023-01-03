49ba29aaeb0289a7e1b1861aa3f16e48bf12f2c2
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-01-03 20:27:44 +0200

    Use new store subscribing

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
   import Navbar from './components/Navbar/Index.svelte';
-  import Main from './components/Main/Index.svelte';
+  import Main from './components/Main/Router.svelte';
   import BackToTop from "./components/BackToTop.svelte";
+  import titles from "./jsons/titles.json";
 
 	import { Router } from "svelte-navigator";
   import { title } from "./stores";
-
-  let titleValue: string = "Katsu Nikki";
-	title.subscribe((value: string) => { titleValue = value });
 </script>
 
 <svelte:head>
   <title>{
-    titleValue == "Katsu Nikki" ? "Katsu Nikki" : `Katsu Nikki | ${titleValue}`
+    $title == titles.mainTitle
+      ? titles.mainTitle
+      : `${titles.mainTitle} | ${$title}`
   }</title>
 </svelte:head>
 
