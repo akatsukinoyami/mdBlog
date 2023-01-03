eda190b178f53ce4b25957f7d031776c4f556312
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-01-03 17:06:43 +0200

    Move functionality to other component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import Navbar from './components/Navbar/Index.svelte';
-  import Aside from './components/Aside/Index.svelte';
-  import Post from './components/Main/Post.svelte';
-  import Section from "./components/Main/Section.svelte";
+  import Main from './components/Main/Index.svelte';
   import BackToTop from "./components/BackToTop.svelte";
 
-	import { Router, Route } from "svelte-navigator";
+	import { Router } from "svelte-navigator";
   import { title } from "./stores";
 
   let titleValue: string = "Katsu Nikki";
@@ -20,34 +18,6 @@
 
 <Router primary={false}>
   <Navbar/>
-  <h1 class="text-center fw-lighter mt-3">{titleValue}</h1>
-  <section class="shadow p-4 my-4 mx-auto rounded-5 bg-white">
-    <main class="pb-4">
-        <Route path="blog/:section/:post" let:params>
-          <Post link="/blog/{params.section}/{params.post}/index.md" />
-        </Route>
-        <Route path="blog/:section" let:params>
-          <Section sectionName={params.section} />
-        </Route>
-        <Route path="page/:page" let:params>
-          <Post link="/page/{params.page}.md" />
-        </Route>
-        <Route>
-          <Post link="/page/index.md" />
-        </Route>
-    </main>
-    <Aside/>
-  </section>
+  <Main />
   <BackToTop />
 </Router>
-
-<style lang="sass">
-  *
-    font-family: 'Merriweather', Georgia, Times, serif
-
-  section
-    max-width: 950px
-
-  h1
-    text-shadow: 0px 0px 4px white
-</style>
---
+++ src/components/Main/Index.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Aside from '../Aside/Index.svelte';
+  import Post from './Post.svelte';
+  import Section from "./Section.svelte";
+
+	import { Route } from "svelte-navigator";
+  import { title } from "../../stores";
+
+  let titleValue: string = "Katsu Nikki";
+	title.subscribe((value: string) => { titleValue = value });
+</script>
+
+
+<h1 class="text-center fw-lighter mt-3">{titleValue}</h1>
+<section class="shadow p-4 my-4 mx-auto rounded-5 bg-white">
+  <main class="pb-4">
+
+      <Route path="blog/:section/:post" let:params>
+        <Post link="/blog/{params.section}/{params.post}/index.md" />
+      </Route>
+
+      <Route path="blog/:section" let:params>
+        <Section sectionName={params.section} />
+      </Route>
+
+      <Route path="page/:page" let:params>
+        <Post link="/page/{params.page}.md" />
+      </Route>
+
+      <Route>
+        <Post link="/page/index.md" />
+      </Route>
+
+  </main>
+  <Aside/>
+</section>
+
+<style lang="sass">
+  *
+    font-family: 'Merriweather', Georgia, Times, serif
+
+  section
+    max-width: 950px
+
+  h1
+    text-shadow: 0px 0px 4px white
+</style>
