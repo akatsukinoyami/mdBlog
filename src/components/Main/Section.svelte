3ba3d4288688cb2cf7a46610f4e6d53a30708b96
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-01-03 20:29:00 +0200

    fixed section for new card component

--- src/components/Main/Section.svelte
+++ src/components/Main/Section.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
   import posts from "../../jsons/posts.json";
-  import PostCard from "./PostCard.svelte";
+  import Card from "../Card.svelte";
   import { updateTitle } from "../../functions";
+  import type DirectoryTreeFileInterface from "../../interfaces/directory.tree.file.interface";
 
   export let sectionName: string;
 
   const section = posts.children.find(section => section.name === sectionName)
   updateTitle(sectionName)
+
+  function getLink(post: DirectoryTreeFileInterface): string {
+    return `${section.name}/${post.name}`;
+  }
 </script>
 
 <div class="row">
   {#each section.children as post}
-    {#if post.hasOwnProperty('children')}
-      <PostCard {section} {post} />
+    {#if post?.children}
+      <Card link={getLink(post)} card={post} />
     {/if}
   {/each}
 </div>
