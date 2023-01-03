82a80f8f28061dec431547145a733c867d56bc29
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-01-03 20:34:50 +0200

    changes in card css

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -9,10 +9,10 @@
   const cardName = nameHumanize(card.name);
 </script>
 
-<div class="card m-1 p-1">
+<div class="card m-1 pt-1">
   <!-- svelte-ignore a11y-missing-attribute -->
-  <img src="/blog/{link}/index.jpg" alt="Card image for post {cardName}.">
-  <div class="card-body">
+  <img src="/blog/{link}/index.jpg" class="rounded" alt="Card image for post {cardName}.">
+  <div class="card-body p-2">
     <a href="/blog/{link}" class="text-dark">
       <h5 class="card-title">{cardName}</h5>
     </a>
@@ -26,7 +26,14 @@
 
   .card
     width: 32%
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)
+    border: none
+    transition: 0.3s
 
-  a
-    text-decoration: none
+    &:hover
+      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2)
+
+  .card-body
+    a
+      text-decoration: none
 </style>
