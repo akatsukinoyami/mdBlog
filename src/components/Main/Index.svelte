88e3ca728adf7579934fd1213bec87695dcc66c6
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-01-03 20:29:20 +0200

    Made new index page

--- src/components/Main/Index.svelte
+++ src/components/Main/Index.svelte
@@ -1,47 +1,19 @@
 <script lang="ts">
-  import Aside from '../Aside/Index.svelte';
-  import Post from './Post.svelte';
-  import Section from "./Section.svelte";
+  import posts from "../../jsons/posts.json";
+  import Card from "../Card.svelte";
+  import Post from "./Post.svelte";
+  import { updateTitle } from "../../functions";
 
-	import { Route } from "svelte-navigator";
-  import { title } from "../../stores";
+  let titleValue: string = "Katsu's Nikki";
 
-  let titleValue: string = "Katsu Nikki";
-	title.subscribe((value: string) => { titleValue = value });
+  updateTitle(titleValue)
 </script>
 
-
-<h1 class="text-center fw-lighter mt-3">{titleValue}</h1>
-<section class="shadow p-4 my-4 mx-auto rounded-5 bg-white">
-  <main class="pb-4">
-
-      <Route path="blog/:section/:post" let:params>
-        <Post link="/blog/{params.section}/{params.post}/index" />
-      </Route>
-
-      <Route path="blog/:section" let:params>
-        <Section sectionName={params.section} />
-      </Route>
-
-      <Route path="page/:page" let:params>
-        <Post link="/page/{params.page}" />
-      </Route>
-
-      <Route>
-        <Post link="/page/index" />
-      </Route>
-
-  </main>
-  <Aside/>
-</section>
-
-<style lang="sass">
-  *
-    font-family: 'Merriweather', Georgia, Times, serif
-
-  section
-    max-width: 950px
-
-  h1
-    text-shadow: 0px 0px 4px white
-</style>
+<Post link="/page/index" />
+<div class="row mt-4">
+  {#each posts.children as section}
+    {#if section?.children}
+      <Card link={section.name} card={section} />
+    {/if}
+  {/each}
+</div>
