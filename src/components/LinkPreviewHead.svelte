b78549d5d952611b60fb09b5da1bd4353be22dcb
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-01-03 22:41:04 +0200

    Add component for out services link previews

---
+++ src/components/LinkPreviewHead.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { nameHumanize } from "../functions";
+  import titles from "../jsons/titles.json";
+
+  export let
+    title: string,
+    description: string,
+    imgUrl: string;
+
+    title = nameHumanize(title)
+    title = `${titles.mainTitle} | ${title}`
+</script>
+
+
+<svelte:head>
+  <meta name="description" content={description}>
+
+  <meta property="og:title" content={title}>
+  <meta property="og:site_name" content={titles.mainTitle}>
+  <meta property="og:description" content={description}>
+  <meta property="og:image" content={imgUrl}>
+
+  <meta name="twitter:title" content={title}>
+  <meta name="twitter:description" content={description}>
+  <meta name="twitter:image" content={imgUrl}>
+</svelte:head>
