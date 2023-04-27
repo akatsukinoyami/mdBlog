11e040d09551bc7712c842b8e4b3cfaf5c12ccd1
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-04-27 22:25:57 +0300

    replace Bootstrap with PicoCSS and add fingerprinting to builds

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,20 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
-import css from 'rollup-plugin-css-only';
-import copy from "rollup-plugin-copy";
+import css  from 'rollup-plugin-css-only';
+import del  from "rollup-plugin-delete";
 import json from "@rollup/plugin-json";
+import replaceHtmlVars  from "rollup-plugin-replace-html-vars";
+import copy from "rollup-plugin-copy";
 
-const production = !process.env.ROLLUP_WATCH;
+const
+  production = !process.env.ROLLUP_WATCH,
+  css_framework = "node_modules/@picocss/pico/css/pico.classless.min.css",
+  unixTime = new Date().valueOf();
+
+function fingerprint(name) {
+  return `${unixTime}.${name}`
+}
 
 function serve() {
 	let server;
@@ -39,7 +48,8 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+    dir: "./public/build",
+		entryFileNames: fingerprint('bundle.js')
 	},
 	plugins: [
 		svelte({
@@ -47,14 +57,18 @@ export default {
 			compilerOptions: { dev: !production } // enable run-time checks when not in production
 		}),
     copy({
+      verbose: true,
       targets: [
-        { src: 'node_modules/bootstrap-icons/font/fonts/**', dest: 'public/build/fonts' },
-        { src: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', dest: 'public/build' },
-        { src: 'node_modules/bootstrap/dist/css/bootstrap.min.css.map', dest: 'public/build' }
+        { src: css_framework, dest: './public/build', rename: (name, extension) => fingerprint(`${name}.${extension}`) },
+        { src: `${css_framework}.map`, dest: './public/build', }
       ],
-      verbose: true
     }),
-    css({ output: 'bundle.css' }), // We'll extract any component CSS out into a separate file - better for performance
+
+    // we"ll extract any component CSS out into a separate file - better for performance
+    css({
+      output: fingerprint('bundle.css'),
+      sourceMap: true
+    }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -62,6 +76,20 @@ export default {
 		// consult the documentation for details:
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
     commonjs(),
+
+    // Replace fingerprints of prebuilt bundle in index.html
+    replaceHtmlVars({
+      files: "./public/index.html",
+      from: /[0-9]{13,13}/gi,
+      to: unixTime
+    }),
+
+    // Delete old build
+    del({
+      targets: './public/build/*',
+      force: true,
+      verbose: true,
+    }),
 
     resolve({ browser: true, dedupe: ['svelte'] }),
 		typescript({ sourceMap: !production, inlineSources: !production }),
