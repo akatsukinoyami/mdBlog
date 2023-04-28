4e8be1e9b746f3c9fc0bb7aab0017f9eb5f93f12
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-04-28 23:40:28 +0300

    clean rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -9,11 +9,9 @@ import css  from 'rollup-plugin-css-only';
 import del  from "rollup-plugin-delete";
 import json from "@rollup/plugin-json";
 import replaceHtmlVars  from "rollup-plugin-replace-html-vars";
-//import copy from "rollup-plugin-copy";
 
 const
   production = !process.env.ROLLUP_WATCH,
-  //css_framework = "node_modules/purecss/build/pure-min.css",
   unixTime = new Date().valueOf();
 
 function fingerprint(name) {
@@ -45,7 +43,7 @@ function serve() {
 export default {
 	input: 'src/main.ts',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
     dir: "./public/build",
@@ -56,16 +54,9 @@ export default {
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: { dev: !production } // enable run-time checks when not in production
 		}),
-    // copy({
-    //   verbose: true,
-    //   targets: [
-    //     { src: css_framework, dest: './public/build', rename: (name, extension) => fingerprint(`${name}.${extension}`) },
-    //     { src: `${css_framework}.map`, dest: './public/build', }
-    //   ],
-    // }),
 
     // we"ll extract any component CSS out into a separate file - better for performance
-    css({ output: fingerprint('bundle.css'), sourceMap: true }),
+    css({ output: fingerprint('bundle.css'), sourceMap: !production }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -82,7 +73,7 @@ export default {
 
     resolve({ browser: true, dedupe: ['svelte'] }),
 		typescript({ sourceMap: !production, inlineSources: !production }),
-    json({ compact: true }),
+    json({ compact: production }),
 		!production && serve(),               // In dev mode, call `npm run start` once the bundle has been generated
 		!production && livereload('public'),  // Watch the `public` directory and refresh the browser on changes when not in production
 		production && terser()                // If we're building for production (npm run build instead of npm run dev), minify
