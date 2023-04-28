916b7019598e09d74fc4cf0616e8708dbe852520
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-04-28 23:39:04 +0300

    reduce size of posts information

--- scripts/inspectMdFiles.js
+++ scripts/inspectMdFiles.js
@@ -1,14 +1,32 @@
 // Script for creating posts tree
 
+const deletableFiles = [".DS_Store", "images", "index.jpg", "index.json", "index.md"];
+
+function deleteDS(children) {
+  return children
+    .filter(child => !deletableFiles.includes(child.name))
+    .map(child => {
+      if (child.children) child.children = deleteDS(child.children);
+      if (child.children.length === 0) {
+        delete child.children;
+      }
+      return child;
+    })
+    .filter(child => child !== undefined)
+}
+
+
 function main() {
   const dirTree = require("directory-tree");
   const fs = require('fs');
 
   const
     treePath = "./public/blog",
-    filePath = "./src/jsons/posts.json";
+    tree = dirTree(treePath),
+    sections = deleteDS(tree.children)
+    filePath = "./src/jsons/sections.json";
 
-  fs.writeFile(filePath, JSON.stringify(dirTree(treePath)), (err) => console.error(err))
+  fs.writeFile(filePath, JSON.stringify(sections), (err) => console.error(err))
   console.log(`Wrote file structure of ${treePath} to ${filePath}`)
 }
 
