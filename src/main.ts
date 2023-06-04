a4d2f88d2fbf619b45882dfd4940e980669c0657
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-05 00:39:48 +0300

    install styles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-export * as css from '../node_modules/chota/dist/chota.min.css';
+import "carbon-components-svelte/css/all.css";
 import App from './App.svelte';
 
 export default new App({ target: document.body });
