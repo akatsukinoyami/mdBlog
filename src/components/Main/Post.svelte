2e1bdc4d3975587771293b803cd7de10a1b2c486
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-05 16:48:59 +0300

    Made Html parsing smarter

--- src/components/Main/Post.svelte
+++ src/components/Main/Post.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+  import { InlineLoading } from "carbon-components-svelte";
+
+  import HtmlContent from "../Partials/HtmlContent.svelte";
+  import { lang } from "../../stores";
   import parseMarkdown from "../../markdown.parser";
   import { i18n, updateTitle } from "../../functions";
   import type PostMetadataInterface from "../../types/post.metadata";
+
+  $: t = i18n($lang);
 
   export let link: string = "/page/index";
 
   async function fetchPost(link: string): Promise<Record<any, any>> {
     const { title, description } = await handleMetadata(link);
-    let content = await handleContent(link);
+    const content = await handleContent(link);
 
     return { title, description, content }
   };
@@ -16,8 +22,8 @@
     const responseMetadata = await fetch(`${link}.json`);
     const metadata = await responseMetadata.json();
 
-    const title = metadata.title || i18n('mainTitle')
-    const description = metadata.description || i18n('fallbackDescription');
+    const title = metadata.title || t.mainTitle;
+    const description = metadata.description || t.fallbackDescription;
 
     updateTitle(title);
     return { title, description };
@@ -26,34 +32,19 @@
   async function handleContent(link: string): Promise<string> {
     const responsePost = await fetch(`${link}.md`);
     const markdown = await responsePost.text();
-    let content = parseMarkdown(markdown);
-    content = fixImages(link, content)
-
-    return content;
+    return parseMarkdown(markdown);
   };
 
-  function fixImages(link: string, content: string): string {
-    link = link.split("/").slice(0, -1).join('/'); // making link from '/blog/section/post/index' to '/blog/section/post'
-
-    return content.replaceAll(/<img(.*?)src="(.*?)"(.*?)alt="(.*?)"(.*?)>/g, `
-      <figure>
-        <img $1 src="${link}/$2" class="modal-image" $3 alt="$4" $5 style="object-fit: contain;" id="$2" loading="lazy" decoding="async"/>
-        <figcaption class="figure-caption modal_caption text-center mx-auto pt-2" style="max-width: 960px"> $4 </figcaption>
-      </figure>
-    `)
-  }
+  
 </script>
 
 
 {#await fetchPost(link)}
-  <div class="d-flex align-items-center">
-    <strong>Please wait, post fetching...</strong>
-    <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-  </div>
-{:then { title, description, content }}
-  <article class="p-3">{@html content}</article>
+  <InlineLoading description={ t.wait } />
+{:then { content }}
+  <HtmlContent html={content} link={link}/>
 {:catch error}
-    <p style="color: red">{error.message}</p>
+  <InlineLoading status="error" description={error.message} />
 {/await}
 
 <style lang="sass">
@@ -65,7 +56,7 @@
     position: fixed
     top: 50%
     left: 50%
-    z-index: 16
+    z-index: 10001
     max-width: 95vw !important
     max-height: 95vh !important
     transform: translate(-50%, -50%)
---
+++ src/components/Partials/HtmlContent.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { StructuredList, StructuredListBody, StructuredListCell, StructuredListHead, StructuredListRow } from "carbon-components-svelte";
+	import { Html, isTag } from 'html-svelte-parser';
+  
+  import Image from "./Image.svelte";
+
+  export let 
+    link: string,
+    html: string;
+
+  function fixImage(src: string): string {
+    return `${link}/${src}`.replace('index/', '');
+  }
+</script>
+
+<Html html={ html } processNode={ (node) => {
+  if (!isTag(node)) return;
+  switch(node.name){
+    case 'img':   return { component: Image, props: { src: fixImage(node.attribs.src), alt: node.attribs.alt } };
+    case 'table': return { component: StructuredList, props: { style: "max-width: var(--content-width) !important;"}};
+    case 'thead': return { component: StructuredListHead };
+    case 'tbody': return { component: StructuredListBody };
+    case 'tr':    return { component: StructuredListRow };
+    case 'th':    return { component: StructuredListCell, props: { head: true }};
+    case 'td':    return { component: StructuredListCell };
+  }
+}} />
