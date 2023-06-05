8d2c00243c527bf5b4d84bd468f04fd4ef40531c
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-05 16:48:01 +0300

    reworked App for CDS

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,33 @@
 <script lang="ts">
+	import { Router, links } from "svelte-navigator";
+  import { LocalStorage } from 'carbon-components-svelte';
+
   import Navbar from './components/Navbar.svelte';
   import Main from './components/Index.svelte';
   import ImageModal from './components/Partials/ImageModal.svelte';
-  import BackToTop from "./components/Partials/BackToTop.svelte";
+
   import { i18n } from "./functions";
-
-	import { Router } from "svelte-navigator";
   import { title, theme, lang } from "./stores";
-  import Footer from './components/Footer.svelte';
 
-  let storedTitle: string, displayedTitle: string;
-  $: {
-    localStorage.lang = $lang;
-    storedTitle = i18n('mainTitle', $lang);
-    displayedTitle = $title == storedTitle ? storedTitle : `${storedTitle} | ${$title}`
-  }
-  $: {
-    localStorage.theme = $theme;
-    document.body.classList.remove('dark', 'light')
-    document.body.classList.add($theme)
-    }
+  $: t = i18n($lang);
+  $: displayedTitle = $title == t.mainTitle ? t.mainTitle : `${t.mainTitle} | ${$title}`;
+  $: document.documentElement.setAttribute("theme", $theme);
+
 </script>
 
 <svelte:head>
   <title>{ displayedTitle }</title>
 </svelte:head>
 
-<svelte:body style="background-color: #821475;"></svelte:body>
+<div use:links>
+  <Router primary={false}>
+    <ImageModal />
+    <Navbar/>
+    <Main />
+  </Router>
+  
+  <LocalStorage key="lang" bind:value={$lang} />
+  <LocalStorage key="theme" bind:value={$theme} />
+</div>
 
-<Router primary={false}>
-  <ImageModal />
-  <Navbar/>
-  <Main />
-  <Footer/>
-  <BackToTop />
-</Router>
-
-
-<style lang="sass">
-  @import './app.sass'
-</style>
+<style lang="sass">@use "app"</style>
