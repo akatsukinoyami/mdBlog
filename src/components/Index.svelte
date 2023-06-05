d5409eb8b449f9e8e3dbba4b318145992ae38907
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-05 16:51:48 +0300

    update Index for CDS

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Tile } from "carbon-components-svelte";
+
   import Index from './Main/Index.svelte';
   import Post from './Main/Post.svelte';
   import Section from "./Main/Section.svelte";
@@ -7,40 +9,36 @@
   import { title } from "../stores";
 </script>
 
-<h1 class="text-center">{$title}</h1>
+
 <section>
-  <main>
+  <h1 class="text-center">{$title}</h1>
 
-      <Route path="blog/:section/:post" let:params>
-        <Post link="/blog/{params.section}/{params.post}/index" />
-      </Route>
-
-      <Route path="blog/:section" let:params>
-        <Section sectionName={params.section} />
-      </Route>
-
-      <Route path="page/:page" let:params>
-        <Post link="/page/{params.page}" />
-      </Route>
-
-      <Route>
-        <Index />
-      </Route>
-
-  </main>
+  <Tile>
+    <Route path="blog/:section/:post" let:params>
+      <Post link="/blog/{params.section}/{params.post}/index" />
+    </Route>
+  
+    <Route path="blog/:section" let:params>
+      <Section sectionName={params.section} />
+    </Route>
+  
+    <Route path="page/:page" let:params>
+      <Post link="/page/{params.page}" />
+    </Route>
+  
+    <Route>
+      <Index />
+    </Route>
+  </Tile>
 </section>
 
 <style lang="sass">
-  h1
-    text-shadow: 0px 0px 4px var(--bg-color)
-
   section
-    max-width: 1000px
+    max-width: var(--content-width)
     margin: 20px auto 100px
     padding: 20px
-    background-color: var(--color-light-grey)
     border-radius: 10px
 
-  main
-    padding-bottom: 20px
+    h1
+      margin: 30px
 </style>
