77d31305da8b805edefd7eb4b1799b816ecda28f
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-05 16:51:58 +0300

    update navbar for CDS

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -25,28 +25,11 @@
   $: t = i18n($lang);
 </script>
 
-<Header company={ t.mainTitle } bind:isSideNavOpen>
+<Header company={ t.mainTitle } href="/" bind:isSideNavOpen>
   <svelte:fragment slot="skip-to-content">
     <SkipToContent />
   </svelte:fragment>
-  <HeaderUtilities>
-    <OverflowMenu size="xl" flipped>
-
-      <OverflowMenuItem text={ t.theme.singular } disabled hasDivider />
-      {#each $themes as themeCode}
-        <OverflowMenuItem on:click={ () => theme.set(themeCode) }>
-          { t.theme[themeCode] }
-        </OverflowMenuItem>
-      {/each}
-
-      <OverflowMenuItem text={ t.lang.singular } disabled hasDivider />
-      {#each $langs as langCode}
-        <OverflowMenuItem on:click={ () => lang.set(langCode) }>
-          { t.lang[langCode] }
-        </OverflowMenuItem>
-      {/each}
-    </OverflowMenu>
-    
+  <HeaderUtilities> 
     <HeaderAction bind:isOpen={ isOpenPages } text={ t.links } icon={ Link }>
       <HeaderPanelLinks>
         <HeaderPanelDivider>{ t.pages }</HeaderPanelDivider>
@@ -58,14 +41,33 @@
 
         <HeaderPanelDivider>{ t.myResources }</HeaderPanelDivider>
         {#each linksJson.my as { link, title }}
-          <HeaderPanelLink href={ link }>{ title }</HeaderPanelLink>
+          <HeaderPanelLink href={ link } target="_blank">{ title }</HeaderPanelLink>
         {/each}
 
         <HeaderPanelDivider>{ t.external }</HeaderPanelDivider>
         {#each linksJson.external as { link, title }}
-          <HeaderPanelLink href={ link }>{ title }</HeaderPanelLink>
+          <HeaderPanelLink href={ link } target="_blank">{ title }</HeaderPanelLink>
         {/each}
         
+      </HeaderPanelLinks>
+    </HeaderAction>
+
+    <HeaderAction bind:isOpen={ isOpenSettings } icon={ SettingsAdjust }>
+      <HeaderPanelLinks>
+
+        <HeaderPanelDivider>{ t.theme.singular }</HeaderPanelDivider>
+        {#each $themes as themeCode}
+          <HeaderPanelLink on:click={ () => theme.set(themeCode) }>
+            { t.theme[themeCode] }
+          </HeaderPanelLink>
+        {/each}
+
+        <HeaderPanelDivider>{ t.lang.singular }</HeaderPanelDivider>
+        {#each $langs as langCode}
+          <HeaderPanelLink on:click={ () => lang.set(langCode) }>
+            { t.lang[langCode] }
+          </HeaderPanelLink>
+        {/each}
       </HeaderPanelLinks>
     </HeaderAction>
   </HeaderUtilities>
