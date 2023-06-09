a3793d83c40b95d0c1212af071dcc35b142547f3
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-09 20:42:11 +0300

    make posts saved in app on compile

--- scripts/inspectMdFiles.js
+++ scripts/inspectMdFiles.js
@@ -3,7 +3,7 @@
 const dirTree = require("directory-tree");
 const fs = require('fs');
 
-const deletableFiles = [".DS_Store", "images", "index.jpg", "index.json", "index.md"];
+const deletableFiles = /(\.DS_Store)|(images)|(index(_\w{2})?\.(jpe?g|png|webp|json|md))/mi;
 
 function sortByDate(a, b) {
   if (!a.date || !b.date) return 0;
@@ -16,19 +16,26 @@ function sortByDate(a, b) {
   return 0
 }
 
-function deleteDS(children) {
+function fetchContent(path, lang) {
+  try {
+    return fs.readFileSync(`./${path}/index${lang}.md`, "utf-8", (err) => { console.log(err) });
+  } catch {
+
+  }
+}
+
+function handlePosts(children) {
   return children
-    .filter(child => !deletableFiles.includes(child.name))
+    .filter(child => !deletableFiles.test((child.name)))
     .map(child => {
-      if (child.children) child.children = deleteDS(child.children);
-      if (child.children.length === 0) {
-        delete child.children;
-      }
-      child = {
-        ...child, 
-        ...require(`../${child.path}/index.json`)
-      };
+      if (child.children) child.children = handlePosts(child.children);
+      if (child.children?.length === 0) delete child.children;
       
+      child = { ...child,  ...require(`../${child.path}/index.json`) };
+      
+      ["", "_en", "_ru", "_ua"].forEach(lang => (
+        child[`content${lang}`] = fetchContent(child.path, lang)
+      ))
       try {
         child.content = fs.readFileSync(`./${child.path}/index.md`, "utf-8", (err) => { console.log(err) });
       } catch {}
@@ -37,7 +44,6 @@ function deleteDS(children) {
     .filter(child => child !== undefined)
     .sort(sortByDate)
     .reverse()
-    
 }
 
 
@@ -45,7 +51,7 @@ function main() {
   const
     treePath = "./public/blog",
     tree = dirTree(treePath),
-    sections = deleteDS(tree.children)
+    sections = handlePosts(tree.children)
     filePath = "./src/jsons/sections.json";
 
   fs.writeFile(filePath, JSON.stringify(sections), (err) => console.error(err))
