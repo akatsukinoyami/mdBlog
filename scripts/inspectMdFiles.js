01fdde16f8e44a97051020aa08bab5975f1a90bf
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-09 17:43:35 +0300

    Posts saved in bundle

--- scripts/inspectMdFiles.js
+++ scripts/inspectMdFiles.js
@@ -1,6 +1,20 @@
 // Script for creating posts tree
 
+const dirTree = require("directory-tree");
+const fs = require('fs');
+
 const deletableFiles = [".DS_Store", "images", "index.jpg", "index.json", "index.md"];
+
+function sortByDate(a, b) {
+  if (!a.date || !b.date) return 0;
+
+  a = new Date(a.date);
+  b = new Date(b.date);
+
+  if (a > b)   return 1
+  if (a < b)   return -1
+  return 0
+}
 
 function deleteDS(children) {
   return children
@@ -10,16 +24,24 @@ function deleteDS(children) {
       if (child.children.length === 0) {
         delete child.children;
       }
+      child = {
+        ...child, 
+        ...require(`../${child.path}/index.json`)
+      };
+      
+      try {
+        child.content = fs.readFileSync(`./${child.path}/index.md`, "utf-8", (err) => { console.log(err) });
+      } catch {}
       return child;
     })
     .filter(child => child !== undefined)
+    .sort(sortByDate)
+    .reverse()
+    
 }
 
 
 function main() {
-  const dirTree = require("directory-tree");
-  const fs = require('fs');
-
   const
     treePath = "./public/blog",
     tree = dirTree(treePath),
