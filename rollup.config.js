eaa08b59aaa71933194747426fa532886ce4a3ff
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-17 19:07:52 +0300

    fix indentation problems

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import commonjs 			from "@rollup/plugin-commonjs";
-import json 				from "@rollup/plugin-json";
-import resolve 				from "@rollup/plugin-node-resolve";
-import typescript 			from "@rollup/plugin-typescript";
-import css  				from "rollup-plugin-css-only";
-import del  				from "rollup-plugin-delete";
-import livereload 			from "rollup-plugin-livereload";
-import replaceHtmlVars 		from "rollup-plugin-replace-html-vars";
-import svelte 				from "rollup-plugin-svelte";
-import { terser } 			from "rollup-plugin-terser";
+import commonjs 			      from "@rollup/plugin-commonjs";
+import json 				        from "@rollup/plugin-json";
+import resolve 				      from "@rollup/plugin-node-resolve";
+import typescript 			    from "@rollup/plugin-typescript";
+import css  				        from "rollup-plugin-css-only";
+import del  				        from "rollup-plugin-delete";
+import livereload 			    from "rollup-plugin-livereload";
+import replaceHtmlVars 		  from "rollup-plugin-replace-html-vars";
+import svelte 				      from "rollup-plugin-svelte";
+import { terser } 			    from "rollup-plugin-terser";
+import sveltePreprocess 	  from "svelte-preprocess";
 import { optimizeImports } 	from "carbon-preprocess-svelte";
-import sveltePreprocess 	from "svelte-preprocess";
 
 const
   production = !process.env.ROLLUP_WATCH,
@@ -60,25 +60,25 @@ export default {
 			compilerOptions: { dev: !production } // enable run-time checks when not in production
 		}),
 
-    // we"ll extract any component CSS out into a separate file - better for performance
-    css({ output: fingerprint("bundle.css"), sourceMap }),
+  // we"ll extract any component CSS out into a separate file - better for performance
+  css({ output: fingerprint("bundle.css"), sourceMap }),
 
 	// If you have external dependencies installed from
 	// npm, you'll most likely need these plugins. In
 	// some cases you'll need additional configuration -
 	// consult the documentation for details:
 	// https://github.com/rollup/plugins/tree/master/packages/commonjs
-    commonjs(),
+  commonjs(),
 
-    // Replace fingerprints of prebuilt bundle in index.html
-    replaceHtmlVars({ files: "./public/index.html", from: /[0-9]{13,13}/gi, to: unixTime }),
+  // Replace fingerprints of prebuilt bundle in index.html
+  replaceHtmlVars({ files: "./public/index.html", from: /[0-9]{13,13}/gi, to: unixTime }),
 
-    // Delete old build
-    del({ targets: "./public/build/*", force: true, verbose: true, }),
+  // Delete old build
+  del({ targets: "./public/build/*", force: true, verbose: true, }),
 
-    resolve({ browser: true, dedupe: ["svelte"] }),
+  resolve({ browser: true, dedupe: ["svelte"] }),
 	typescript({ sourceMap, inlineSources: sourceMap }),
-    json({ compact: production }),
+  json({ compact: production }),
 	!production && serve(),               // In dev mode, call `npm run start` once the bundle has been generated
 	!production && livereload("public"),  // Watch the `public` directory and refresh the browser on changes when not in production
 	production && terser()                // If we"re building for production (npm run build instead of npm run dev), minify
