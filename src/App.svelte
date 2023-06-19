8aec551848d2920175bb72edf2578a30aa387a21
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-19 18:07:28 +0300

    small fixes

--- src/App.svelte
+++ src/App.svelte
@@ -3,25 +3,30 @@
   import { LocalStorage } from 'carbon-components-svelte';
 
   import Routes from './Routes.svelte';
-  import Navbar from './components/Partials/Navbar.svelte';
+  import Navbar from './components/Partials/Navbar/Index.svelte';
   import BackToTop from "./components/Partials/BackToTop.svelte";
-  import ImageModal from "./components/Partials/ImageModal.svelte";
-  import { theme, lang, imgMode } from "./stores";
+  import ImageModal from "./components/Partials/Modal/Image.svelte";
+  import { backend } from "./stores/backend";
+  import { theme, lang, imgMode } from "./stores/index";
 
   $: document.documentElement.setAttribute("theme", $theme);
+  
+  $backend.check()
 </script>
 
 <div use:links>
   <Router primary={false}>
     <Navbar/>
     <Routes />
+
+    <ImageModal/>
+    <BackToTop/>
+    <LocalStorage key="lang" bind:value={$lang} />
+    <LocalStorage key="theme" bind:value={$theme} />
+    <LocalStorage key="imgMode" bind:value={$imgMode} />
+    <LocalStorage key="token" bind:value={$backend.token} />
+    <LocalStorage key="username" bind:value={$backend.username} />
   </Router>
-  
-  <ImageModal/>
-  <BackToTop/>
-  <LocalStorage key="lang" bind:value={$lang} />
-  <LocalStorage key="theme" bind:value={$theme} />
-  <LocalStorage key="imgMode" bind:value={$imgMode} />
 </div>
 
 <style lang="sass">
