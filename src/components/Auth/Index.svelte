432e0fe2517490d8e0649421acd822cc876cb62c
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-19 18:04:12 +0300

    add auth components

---
+++ src/components/Auth/Index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Route } from "svelte-navigator";
+  
+  import SignIn     from "./SignIn.svelte";
+  import SignUp     from "./SignUp.svelte";
+  import { backend } from "../../stores/backend";
+
+  $: authable = $backend.avaliable;
+</script>
+
+{#if authable}
+  <Route path="auth/*">
+    <Route path="sign_in" component={SignIn} />
+    <Route path="sign_up" component={SignUp} />
+  </Route>
+{/if}
---
+++ src/components/Auth/SignIn.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Button, TextInput } from "carbon-components-svelte";
+  import { updateTitle } from "../../functions";
+  import i18n from "../../i18n";
+  import { lang } from "../../stores/index";
+  import { backend } from "../../stores/backend";
+
+  $: t = i18n($lang)
+  $: updateTitle(t.auth.sign_in)
+
+  let email="";
+  let password="";
+</script>
+
+<TextInput inline size="xl" labelText="Email"    placeholder="Email"    bind:value={email} />
+<TextInput inline size="xl" labelText="Password" placeholder="Password" bind:value={password} />
+<Button on:click={() => $backend.signIn(email, password) }>{ t.auth.sign_in }</Button>
+
