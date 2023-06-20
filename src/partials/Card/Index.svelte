5b4efe3e11aec46039c2772f408553743aa34151
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-20 21:22:55 +0300

    Reworked cards index using css grids

--- src/partials/Card/Index.svelte
+++ src/partials/Card/Index.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { Grid, Row } from "carbon-components-svelte";
   import Card from "./Show.svelte";
   import type { DirectoryTreeFile } from "../../types/directory.tree.file";
 
@@ -8,10 +7,30 @@
     linkPrepare: (card: DirectoryTreeFile) => string;
 </script>
 
-<Grid fullWidth padding>
-  <Row padding={true}>
-    {#each cards as card}
-      <Card link={linkPrepare(card)} card={card} />
-    {/each}
-  </Row>
-</Grid>
+<div class="card__index">
+  {#each cards as card}
+    <Card link={linkPrepare(card)} card={card} />
+  {/each}
+</div>
+
+<style lang="sass">
+  @import "../../styles/breakpoints"
+
+  .card__index
+    display: grid
+    grid-auto-rows: minmax(200px, auto)
+    grid-column-gap: 12px
+    grid-row-gap: 12px
+
+  @media (max-width: $sm) 
+    .card__index
+      grid-template-columns: 1fr
+
+  @media (min-width: $sm) and (max-width: $md)
+    .card__index
+      grid-template-columns: 1fr 1fr
+  
+  @media (min-width: $md) 
+    .card__index
+      grid-template-columns: 1fr 1fr 1fr
+</style>
