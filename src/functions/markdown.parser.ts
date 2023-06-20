8d64ce32f1bdaf9c3facc473689e3e2ec45eeb91
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-06-20 21:21:42 +0300

    move some html-parser funcs to markdown parser

--- src/functions/markdown.parser.ts
+++ src/functions/markdown.parser.ts
@@ -1,5 +1,30 @@
 import showdown from "showdown";
 
+const classMap = {
+  figcaption: 'mt-1',
+  p: 'mb-1',
+  h1: 'mb-1',
+  h2: 'mb-1',
+  h3: 'mb-1',
+  h4: 'mb-1',
+  h5: 'mb-1',
+  h6: 'mb-1',
+}
+
+const bindings = Object
+  .keys(classMap)
+  .map(key => ({
+    type: 'output',
+    regex: new RegExp(`<${key}(.*)>`, 'g'),
+    replace: `<${key} class="${classMap[key]}" $1>`
+  }));
+
+const a = {
+  type: 'output',
+  regex: /<a(.*?)>(.*?)<\/a>/g,
+  replace: '<a $1 class="body_link" target="_blank">$2</a>'
+}
+
 const parser = new showdown.Converter({
   excludeTrailingPunctuationFromURLs: true,
   flavor: "github",
@@ -12,7 +37,8 @@ const parser = new showdown.Converter({
   strikethrough: true,
   tables: true,
   tablesHeaderId: true,
-  tasklists: true
+  tasklists: true,
+  extensions: [...bindings, a]
 });
 
 export default function parseMarkdown(markdown: string): string {
--- src/partials/HtmlParser.svelte
+++ src/partials/HtmlParser.svelte
@@ -8,7 +8,7 @@
     Row, 
     UnorderedList 
   } from "carbon-components-svelte";
-	import { Html, isTag } from "html-svelte-parser";
+	import { Html, isTag, Text } from "html-svelte-parser";
   import Image from "./Image.svelte";
 
   export let html: string, link: string;
@@ -18,33 +18,22 @@
   }
 
   function processNode(node: any) {
-      if (!isTag(node)) return;
-      if (node.name !== "a"){
-      const { style } = node.attribs;
-      const margin = "margin-bottom: 10px; ";
-      node.attribs.style = style ? style + margin : margin;
-      }
-      switch(node.name){
-      case "img":   return { 
-          component: Image, 
-          props: { 
-          src: fixImage(node.attribs.src), 
-          alt: node.attribs.alt, 
-          style: node.attribs.style,
-      }};
-      case "pre":   return { component: CodeSnippet, props: { type: "multi", wrapText: true, style: `${node.attribs.style}background-color: var(--cds-field-01); ` }};
-      //case "thead": return (new Text("i stay!"));
-      //case "tbody": return (new Text("i stay!"));
-      case "ol":    return { component: OrderedList };
-      case "ul":    return { component: UnorderedList };
-      case "li":    return { component: ListItem };
-      case "table": return { component: Grid, props: { style: node.attribs.style, fullWidth: true }};
-      case "tr":    return { component: Row, props: { style: node.attribs.style }};
-      case "th": 
-      case "td":    
-          return { component: Column, props: { style: node.attribs.style } };
-      case "a" :    node.attribs.target = "_blank";
+    if (!isTag(node)) return;
+    
+    const { alt, src, style } = node.attribs;
+    const props = { "class": node.attribs.class, style };
+
+    const components = {
+      img: { 
+        component: Image,        
+        props: { ...props, src: fixImage(src), alt }
+      },
+      pre: { 
+        component: CodeSnippet,  
+        props: { ...props, type: "multi", wrapText: true }
+      },
     }
+    return components[node.name];
   }
 </script>
 
