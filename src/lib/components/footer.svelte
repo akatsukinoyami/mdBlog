2a3f112c12a8790afd39d14b62546fd7ad049959
akatsukinoyami <hirokod3@gmail.com>
2025-06-06 11:58:23 +0300

    change mobile footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -12,13 +12,13 @@
 </script>
 
 <footer class="
-  flex md:block 
-  w-full md:w-inherit
+  flex xl:block 
+  w-full xl:w-inherit
   gap-4 justify-center
-  fixed bottom-0 md:right-6
+  fixed bottom-0 xl:right-6
 ">
   {#each links as [link, icon, label]}
-    <a class={["flex gap-3 justify-end items-center text-xs mb-1 md:mb-4 grayscale hover:grayscale-0 opacity-50  hover:opacity-80", hoverShadows]} href="https://{link}" target="_blank">
+    <a class={["flex gap-3 justify-end items-center text-xs mb-1 xl:mb-4 grayscale hover:grayscale-0 opacity-50  hover:opacity-80", hoverShadows]} href="https://{link}" target="_blank">
       {label} <img class="h-3 w-3" src="/icons/{icon}.svg" alt="{label} icon" />
     </a>
   {/each}
