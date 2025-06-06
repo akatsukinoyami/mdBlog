2bd01f691d3d3b81ef45dc5666fd2ff150d3397e
akatsukinoyami <hirokod3@gmail.com>
2025-06-06 11:55:46 +0300

    add mobile config for footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -11,9 +11,14 @@
   
 </script>
 
-<footer class="fixed bottom-0 right-6">
+<footer class="
+  flex md:block 
+  w-full md:w-inherit
+  gap-4 justify-center
+  fixed bottom-0 md:right-6
+">
   {#each links as [link, icon, label]}
-    <a class={["flex gap-3 justify-end items-center text-xs mb-4 grayscale hover:grayscale-0 opacity-50  hover:opacity-80", hoverShadows]} href="https://{link}" target="_blank">
+    <a class={["flex gap-3 justify-end items-center text-xs mb-1 md:mb-4 grayscale hover:grayscale-0 opacity-50  hover:opacity-80", hoverShadows]} href="https://{link}" target="_blank">
       {label} <img class="h-3 w-3" src="/icons/{icon}.svg" alt="{label} icon" />
     </a>
   {/each}
