69fe5d96b61a2496c3ffd639966660e301a6685e
akatsukinoyami <hirokod3@gmail.com>
2025-06-08 01:42:43 +0300

    added icons to footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,12 +3,19 @@
   import { theme } from "$lib/stores";
 
   const links = $derived([
+    ["t.me/nyawoobot", `telegram`, "Telegram"],
     ["github.com/akatsukinoyami/mdBlog", `github-${$theme}`, "Github"],
     ["svelte.dev", `svelte`, "Svelte"],
     ["tailwindcss.com", `tailwind`, "TailwindCSS"],
+    ["hetzner.com", `hetzner`, "Hetzner"],
+    ["tailwindcss.com", `caprover`, "Caprover"],
+  ]);
 
-  ])
-  
+  const linkClasses = [
+    "hidden md:flex gap-3 justify-end items-center mb-1 xl:mb-4",
+    "text-xs grayscale hover:grayscale-0 opacity-50 hover:opacity-80",
+    hoverShadows
+  ];
 </script>
 
 <footer class="
@@ -18,7 +25,7 @@
   fixed bottom-0 xl:right-6
 ">
   {#each links as [link, icon, label]}
-    <a class={["flex gap-3 justify-end items-center text-xs mb-1 xl:mb-4 grayscale hover:grayscale-0 opacity-50  hover:opacity-80", hoverShadows]} href="https://{link}" target="_blank">
+    <a class={linkClasses} href="https://{link}" target="_blank">
       {label} <img class="h-3 w-3" src="/icons/{icon}.svg" alt="{label} icon" />
     </a>
   {/each}
