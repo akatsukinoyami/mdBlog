f90c0b7efcee51d364de8e6d56db084cf2b88980
akatsukinoyami <hirokod3@gmail.com>
2025-06-12 14:46:48 +0300

    fix db issue

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,6 +1,6 @@
-import { db } from "$lib/db";
+import { sql } from "$lib/db";
 import { json } from "@sveltejs/kit";
 
 export async function GET() {
-  return json(db.query("select 'ara-ara~' as message;").get());
+  return json(await sql`SELECT 'ara-ara~' AS message;`);
 }
