2212bd1931f7b4022912b6960e0500d10a7ac2bb
akatsukinoyami <hirokod3@gmail.com>
2025-06-12 15:40:47 +0300

    update envs

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,10 @@
 import { SQL } from "bun";
 
 export const sql = new SQL({
-  hostname: import.meta.env.VITE_POSTGRES_HOST,
-  port: import.meta.env.VITE_POSTGRES_PORT,
-  database: import.meta.env.VITE_POSTGRES_DB,
-  username: import.meta.env.VITE_POSTGRES_USER,
-  password: import.meta.env.VITE_POSTGRES_PASSWORD,
-  //onconnect: _ => console.log("Connected to database"),
-  //onclose: _ => console.log("Connection closed"),
+  hostname: process.env.POSTGRES_HOST || import.meta.env.VITE_POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT || import.meta.env.VITE_POSTGRES_PORT,
+  database: process.env.POSTGRES_DB || import.meta.env.VITE_POSTGRES_DB,
+  username: process.env.POSTGRES_USER || import.meta.env.VITE_POSTGRES_USER,
+  password:
+    process.env.POSTGRES_PASSWORD || import.meta.env.VITE_POSTGRES_PASSWORD,
 });
